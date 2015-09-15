06908a8227df84c45108dfb7a8c5dd5ea1cd321d
rohitiscancerian <er.rohitkumar@gmail.com>
2015-09-15 21:25:55 +0100

    Testing Tortoise git

--- VGWagers/Models/AccountViewModels.cs
+++ VGWagers/Models/AccountViewModels.cs
@@ -92,7 +92,7 @@ namespace VGWagers.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
+        [DataType(DataType.Password)] 
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
